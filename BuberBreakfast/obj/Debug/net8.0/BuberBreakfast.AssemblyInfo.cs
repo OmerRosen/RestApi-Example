17b6dfc71cf257508aed651b2e19020267a7aba7
Omer Rosen <rosen.omer@gmail.com>
2024-05-22 17:15:13 -0400

    commit 2

--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4a0f0daf3270a885a1021a8d81184974c76ee9c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
