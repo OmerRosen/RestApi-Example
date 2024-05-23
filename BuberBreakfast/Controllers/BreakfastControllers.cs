9005e1b2d1397721b1852014b35b176d246f53e6
Omer Rosen <rosen.omer@gmail.com>
2024-05-22 23:31:37 -0400

    Refactor controller

--- BuberBreakfast/Controllers/BreakfastControllers.cs
+++ BuberBreakfast/Controllers/BreakfastControllers.cs
@@ -1,26 +1,50 @@
 using BuberBreakfast.Contracts;
+using BuberBreakfast.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberBreakfast.Controllers;
 
 [ApiController]
+[Route("breakfests")]
 public class BreakfastControllers : ControllerBase{
-    [HttpPost("/breakfests")]
+    [HttpPost("")]
     public IActionResult CreateBreakfast(CreateBreakfastRequest request){
-        return Ok(request);
+        Breakfast breakfast = new Breakfast(Guid.NewGuid() ,
+                        request.Name ,
+                        request.Description ,
+                        request.StartDateTime ,
+                        request.EndDateTime ,
+                        DateTime.UtcNow,
+                        request.Savory ,
+                        request.Sweet);
+        
+        // ToDo: Savo to database
+
+        GetBreakfastReesponse response = new GetBreakfastReesponse(breakfast.Id ,
+                        breakfast.Name ,
+                        breakfast.Description ,
+                        breakfast.StartDateTime ,
+                        breakfast.EndDateTime ,
+                        breakfast.LastModifiedDateTime,
+                        breakfast.Savory ,
+                        breakfast.Sweet);
+
+        
+        return CreatedAtAction(nameof(CreateBreakfast),
+        value: response);
     }
 
-    [HttpGet("breakfests/{id:guid}")]
+    [HttpGet("{id:guid}")]
     public IActionResult GetBreakfast(Guid id){
         return Ok(id);
     }
 
-    [HttpPut("breakfests/{id:guid}")]
+    [HttpPut("{id:guid}")]
     public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request){
         return Ok(request);
     }
 
-    [HttpDelete("breakfests/{id:guid}")]
+    [HttpDelete("{id:guid}")]
     public IActionResult DeleteBreakfast(Guid id){
         return Ok(id);
     }
--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17b6dfc71cf257508aed651b2e19020267a7aba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ca78509c0f80ec92d06f93ece3a49e0b4a90fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
